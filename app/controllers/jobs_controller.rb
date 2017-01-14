020f94e94a003c559455962e7ed9968d703ae5e3
like <20783506632@qq.com>
2017-01-14 14:42:10 +0800

    ybc

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,6 +2,10 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def show
     @job = Job.find(params[:id])
+    if @job.is_hidden
+      flash[:warning] = "This job already archieved"
+      redirect_to root_path
+    end
   end
 
   def edit
@@ -9,7 +13,14 @@ class JobsController < ApplicationController
   end
 
   def index
-    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+    @jobs = case params[:order]
+             when 'by_lower_bound'
+               Job.where(is_hidden: false).order('wage_lower_bound DESC')
+             when 'by_upper_bound'
+               Job.where(is_hidden: false).order('wage_upper_bound DESC')
+             else
+               Job.where(is_hidden: false).order('created_at DESC')
+             end
   end
 
   def new
