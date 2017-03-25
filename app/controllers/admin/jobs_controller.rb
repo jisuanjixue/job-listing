ab3938f523b108f6f3cfdb464ba0a3ebbcf9b9c1
like <20783506632@qq.com>
2017-03-25 12:17:28 +0800

    implement part5.3

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,8 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
+ layout "admin"
+
     def index
       @jobs = Job.all
     end
@@ -39,6 +41,18 @@ class Admin::JobsController < ApplicationController
        @job = Job.find(params[:id])
        @job.destroy
       redirect_to admin_jobs_path
+    end
+
+   def publish
+     @job = Job.find(params[:id])
+     @job.publish!
+redirect_to :back
+end
+
+  def hide
+    @job = Job.find(params[:id])
+    @job.hide!
+    redirect_to :back
     end
 
       private
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -15,6 +15,10 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
 
   def show
     @job = Job.find(params[:id])
+  if @job.is_hidden
+  flash[:warning] = "This job already archieved"
+redirect_to root_path
+end
   end
 
   def create
