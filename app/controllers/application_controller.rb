bc41d07288bcd0de1be5952ac5f6bd31e5bef1ae
like <20783506632@qq.com>
2017-05-05 21:42:26 +0800

    on any page can devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :store_current_location, :unless => :devise_controller?
+
+  private
+
+def store_current_location
+store_location_for(:user, request.url)
+end
 
   def require_is_admin
       if !current_user.admin?
--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -3,4 +3,17 @@ module JobsHelper
     excerpt_cont = excerpt(job.title, query_string, radius: 500)
     highlight(excerpt_cont, query_string)
   end
+
+  def resource_name
+  :user
+  end
+
+  def resource
+  @resource ||= User.new
+  end
+
+  def devise_mapping
+  @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
